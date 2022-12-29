cfd95ca3045b964dcd98a86c7a3a8540a7d19fb0
HARISREE SURESH <getharisree@outlook.com>
2022-12-29 19:07:50 +0530

    Create Stone_Paper_scissor.py

---
+++ Stone_Paper_scissor.py
@@ -0,0 +1,25 @@
+import random
+score1 = 0
+score2 = 0
+lisst1 = ["stone","paper","scissor"]
+
+while score1 < 5 and score2 < 5:
+    player1 = input(str(("player1 enter your choice ")))
+    player2 = random.choice(lisst1)
+    
+    if player1 == 'stone' and player2 == 'paper':
+        score2 += 1
+        print(player2)
+    elif player1 == 'scissor' and player2 == 'stone':
+        score2 += 1
+        print(player2)
+    elif player1 == 'paper' and player2 == 'scissor':
+        score2 += 1
+        print(player2)
+    else:
+        score1 +=1
+        print(player2)
+
+winner = max(score1,score2)
+
+print("player1 ",score1,"player2 ",score2)
