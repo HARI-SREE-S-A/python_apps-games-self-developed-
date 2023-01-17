9ddc4e78906c7d460693794e2a27ec4ddad6f65e
HARISREE SURESH <getharisree@outlook.com>
2023-01-17 16:34:03 +0530

    Update Stone_Paper_scissor-with_GUI.py

--- Stone_Paper_scissor-with_GUI.py
+++ Stone_Paper_scissor-with_GUI.py
@@ -1,1 +1,57 @@
+from tkinter import *
+from tkinter import messagebox
+import random
 
+
+ 
+
+def play():
+    player2 = "paper"
+    if vari.get() == "stone" and player2 == "paper":
+        Label(root,text = player2,font = ("Calibri(Body)",12,'bold')).place(x = 200,y = 250)
+        
+    
+    
+
+
+
+
+
+root = Tk()
+root.title("game")
+root.geometry("600x600")
+
+
+options = ['stone','paper','scissor']
+
+
+
+
+
+player1 = Label(root,text = "select an option ",font = ('Calibri(Body)',13,'bold'))
+player1.place(x=50,y = 150)
+vari = StringVar()
+vari.set(options[0])
+p_selection = OptionMenu(root,vari,*options)
+p_selection.place(x = 250,y = 150)
+
+
+playy = Button(root,text = "Play",width = 7,font = ("Calibri(Body)",13,'bold'),command = play)
+playy.place(x = 300,y = 200)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
