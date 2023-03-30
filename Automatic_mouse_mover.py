15f8927238de4b9b7c54e45876e3761aca4edc6d
HARISREE SURESH <getharisree@outlook.com>
2023-03-30 20:06:14 +0530

    Update Automatic_mouse_mover.py

--- Automatic_mouse_mover.py
+++ Automatic_mouse_mover.py
@@ -6,8 +6,7 @@ import datetime
 time = int(input("enter the loop in x 100 ")
 lock = int(input("enetr t- sec your screen lock time ")
 
-           
-           
+                    
 x = list(range(200,700))
 y  = list(range(300,500))
 
